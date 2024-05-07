6d0ee452583501206661b3cf77fd80abe70f5be7
devxyn <johnlawrencehermo@gmail.com>
2024-05-07 13:11:27 +0800

    feat: created Navbar

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -1,5 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <Link to='/'>Home</Link>
+      <Link to='/create-recipe'>Create Recipe</Link>
+      <Link to='/saved-recipe'>Saved Recipe</Link>
+      <Link to='/auth'>Login / Register</Link>
+    </nav>
+  );
 };
 
 export default Navbar;
