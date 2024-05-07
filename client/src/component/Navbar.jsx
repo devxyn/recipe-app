9ac7b94e30a1bc8147d4d8f89a8e0c23ae775cd6
devxyn <johnlawrencehermo@gmail.com>
2024-05-07 14:30:10 +0800

    feat: implemented checking if the user is logged in

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -1,12 +1,26 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
 
 const Navbar = () => {
+  const [cookies, setCookies] = useCookies('access_token');
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    setCookies('access_token', '');
+    window.localStorage.removeItem('userID');
+    navigate('/auth');
+  };
+
   return (
     <nav>
       <Link to='/'>Home</Link>
       <Link to='/create-recipe'>Create Recipe</Link>
       <Link to='/saved-recipe'>Saved Recipe</Link>
-      <Link to='/auth'>Login / Register</Link>
+      {!cookies.access_token ? (
+        <Link to='/auth'>Login / Register</Link>
+      ) : (
+        <button onClick={handleLogout}>Logout</button>
+      )}
     </nav>
   );
 };
