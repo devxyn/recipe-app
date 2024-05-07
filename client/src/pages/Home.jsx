73e2a5059f2112ad4f418346f974b43d2bde47f2
devxyn <johnlawrencehermo@gmail.com>
2024-05-07 14:30:33 +0800

    feat: fixed height styling

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,12 +3,12 @@ import Navbar from '../component/Navbar';
 
 const Home = () => {
   return (
-    <>
+    <div className='h-screen'>
       <Navbar />
-      <main>
+      <main className='h-full border'>
         <Outlet />
       </main>
-    </>
+    </div>
   );
 };
 
