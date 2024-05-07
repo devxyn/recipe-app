0db708e0adba1cd08ee320b70072603f3f646301
devxyn <johnlawrencehermo@gmail.com>
2024-05-07 11:55:53 +0800

    feat: connected to the database

--- server/src/index.js
+++ server/src/index.js
@@ -10,4 +10,13 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
+try {
+  await mongoose.connect(
+    `mongodb+srv://admin:${process.env.DB_PASSWORD}@cluster0.oyznd4x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
+  );
+  console.log(`Connected to MongoDB`);
+} catch (error) {
+  console.error(`Error connecting to MongoDB, ${error}`);
+}
+
 app.listen(port, () => console.log(`Server is running in port ${port}`));
