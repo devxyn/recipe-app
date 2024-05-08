183f47f7d2e6be546b024934b96c118c0325272b
devxyn <johnlawrencehermo@gmail.com>
2024-05-08 14:51:06 +0800

    feat: added savedRecipe item in user schema

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -5,6 +5,7 @@ const { Schema, model } = mongoose;
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true, trim: true },
   password: { type: String, required: true },
+  savedRecipes: [{ type: Schema.Types.ObjectId, ref: 'recipes' }],
 });
 
 const User = model('User', userSchema);
