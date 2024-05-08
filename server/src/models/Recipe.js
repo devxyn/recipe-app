80d7e5a6440a82d72a94a1aba153351047ea1f42
devxyn <johnlawrencehermo@gmail.com>
2024-05-08 14:52:06 +0800

    fix: fix mispelled item

--- server/src/models/Recipe.js
+++ server/src/models/Recipe.js
@@ -4,7 +4,7 @@ const { Schema, model } = mongoose;
 
 const recipeSchema = new Schema({
   name: { type: String, required: true },
-  ingridients: [{ type: String, required: true }],
+  ingredients: [{ type: String, required: true }],
   instructions: { type: String, required: true },
   imageUrl: { type: String, required: true },
   cookingTime: { type: Number, required: true },
