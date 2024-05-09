2f54b1f612e2cacb5721d6c26478f040ebd4db73
devxyn <johnlawrencehermo@gmail.com>
2024-05-09 12:11:55 +0800

    feat: created middleware to verify user tokens

---
+++ server/middlewares/verifyToken.js
@@ -0,0 +1,16 @@
+import 'dotenv/config.js';
+
+export const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, (err) => {
+      if (err) {
+        res.status(403).json({ message: 'Invalid token!' });
+      }
+      next();
+    });
+  } else {
+    res.status(401).json({ message: 'Unauthorized user!' });
+  }
+};
